3c05f3a12b7bab2d868027bdfb743b059068d796
Wulfsta <wulfstawulfsta@gmail.com>
2020-05-22 22:31:47 -0400

    attmept fix

--- pkgs/all-packages.nix
+++ pkgs/all-packages.nix
@@ -184,7 +184,8 @@ with pkgs;
     cc = self.hcc-clang-unwrapped;
     extraPackages = [ libstdcxxHook self.hcc-unwrapped ];
     extraBuildCommands = ''
-      echo "-resource-dir=${self.hcc-clang}/resource-root" >> $out/nix-support/cc-cflags
+      ln -s ${self.hcc-clang}/resource-root $out/resource-root
+      echo "-resource-dir=$out/resource-root" >> $out/nix-support/cc-cflags
       echo "--gcc-toolchain=${stdenv.cc.cc}" >> $out/nix-support/cc-cflags
       ln -s $out/bin/clang++ $out/bin/hcc
       for f in $(find ${self.hcc-unwrapped}/bin); do
